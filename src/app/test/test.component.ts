0bce7a3cff7d5ccdc6750181d75f7915cfca49ba
Natalia Shport <shport.natalia@gmail.com>
2019-08-08 02:01:25 -0700

    12 done

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,17 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h2>
-              Welcome {{name}}
-            </h2>
-            <input [(ngModel)]="name" type="text" /> {{name}}
+  template: `
+  <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+  
+  <ng-template #thenBlock>
+    <h2>It is thenBlock</h2>
+  </ng-template>
+  <ng-template #elseBlock>
+    <h2>It is elseBlock</h2>
+  </ng-template>
+
+  <hr/>
+  <h2 *ngIf="displayName; else otherBlock">
+    Welcome to Angular
+  </h2>
+  <ng-template #otherBlock>
+    <h2>
+      Name is hidden
+    </h2>
+  </ng-template>
+            
             `,
   styles: []
 })
 
 export class TestComponent implements OnInit {
-  public name = ""
-  public greeting = ""
+  displayName = false
+
 
   constructor() {}
   ngOnInit() {}
