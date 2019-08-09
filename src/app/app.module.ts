527af6686824bd3d32b686981c174acf90ca367a
Natalia Shport <shport.natalia@gmail.com>
2019-08-09 14:01:15 -0700

    17-services done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,29 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-
     <h2>{{ date }}</h2>
-    <h2>{{ date | date: 'short' }}</h2>
-    <h2>{{ date | date: 'shortDate' }}</h2>
-    <h2>{{ date | date: 'shortTime' }}</h2>
-
-    <h2>{{ name }}</h2>
-    <h2>{{ name | lowercase }}</h2>
-    <h2>{{ name | uppercase}}</h2>
-    <h2>{{ message | titlecase }}</h2>
-    <h2>{{ name | slice:2:5 }}</h2>
-    <h2>{{ person }}</h2>
-    <h2>{{ person | json }}</h2>
-
-    <h2>{{ 5.678 | number:'1.2-3' }}</h2>
-    <h2>{{ 5.678 | number:'3.4-5' }}</h2>
-    <h2>{{ 5.678 | number:'3.1-2' }}</h2>
-    <h2>{{ 0.25 | percent }}</h2>
-
-    <h2>{{ 0.25 | currency }}</h2>
-    <h2>{{ 0.25 | currency: 'GBP' }}</h2>
-    <h2>{{ 0.25 | currency: 'EUR' }}</h2>
-    <h2>{{ 0.25 | currency: 'EUR': 'code' }}</h2>
   `,
   styles: []
 })
@@ -36,10 +14,7 @@ export class TestComponent implements OnInit {
   public name = "Natalia"
   public message = "Welcome to Angular 8"
   public date = new Date()
-  public person = {
-    "firstname": "John",
-    "lastName": "Doe"
-  }
+
   constructor() {}
 
   ngOnInit() {}
