1f7ea22c8f24f64a4ba84a50fc0c5c3bf99770d8
Natalia Shport <shport.natalia@gmail.com>
2019-08-09 00:33:55 -0700

    14-ngfor-directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,20 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <div [ngSwitch]="color">
-      <div *ngSwitchCase="'red'">You picked red color</div>
-      <div *ngSwitchCase="'blue'">You picked blue color</div>
-      <div *ngSwitchCase="'green'">You picked green color</div>
-      <div *ngSwitchDefault>Pick again</div>
-              
+    <div *ngFor="let color of colors; odd as o">
+      <h2 [style.color]=color>{{o}} {{color}}</h2>
     </div>
+    <div *ngFor="let color of colors; last as l">
+      <h2 [style.font-style]="l ? 'italic' : ''">{{l}} {{color}}</h2>
+    </div>
+    <hr/>
+    <div *ngFor="let color of colors; first as f">
+      <h2 [style.text-decoration]="f ? 'underline' : ''">{{f}} {{color}}</h2>
+    </div>
+    <hr/>
+    <div *ngFor="let color of colors; index as i">
+      <h2>{{i}} {{color}}</h2>
+    </div>
+
   `,
 
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public color = "orange"
+  public colors = ["red", "blue", "green", "yellow"]
 
   constructor() {}
 
