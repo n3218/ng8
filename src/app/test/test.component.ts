08c8ef93922a12e230d29cdb9dbf6f167ba5b7f1
Natalia Shport <shport.natalia@gmail.com>
2019-08-09 01:08:00 -0700

    15-components-interaction

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,33 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-test',
   template: `
-    <div *ngFor="let color of colors; odd as o">
-      <h2 [style.color]=color>{{o}} {{color}}</h2>
-    </div>
-    <div *ngFor="let color of colors; last as l">
-      <h2 [style.font-style]="l ? 'italic' : ''">{{l}} {{color}}</h2>
-    </div>
-    <hr/>
-    <div *ngFor="let color of colors; first as f">
-      <h2 [style.text-decoration]="f ? 'underline' : ''">{{f}} {{color}}</h2>
-    </div>
-    <hr/>
-    <div *ngFor="let color of colors; index as i">
-      <h2>{{i}} {{color}}</h2>
-    </div>
+    <h2>{{ "Hello " + name}}</h2>
+    <button (click)="eventFromChild()">Send Event</button>
 
   `,
-
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public colors = ["red", "blue", "green", "yellow"]
+  @Input('parentData') public name;
+  @Output() public childEvent = new EventEmitter();
 
   constructor() {}
 
   ngOnInit() {}
+  eventFromChild() {
+    this.childEvent.emit('Hey, I am an Event from ChildComponent')
+  }
 
 }
